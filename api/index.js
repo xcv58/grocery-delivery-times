222bab9ed47df1e4a0ee3a98c8b3b4d901455db1
xcv58 <i@xcv58.com>
2020-04-06 22:24:30 -0700

    Add inital example from https://zeit.co/blog/serverless-chrome

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const { parse } = require('url');
+const getScreenshot = require('../screenshot');
+
+module.exports = async function (req, res) {
+    const { pathname = '/', query = {} } = parse(req.url, true);
+    const { type = 'png' } = query; // png or jpeg
+    let url = pathname.slice(1);
+    if (!url.startsWith('http')) {
+        url = 'https://' + url; // add protocol if missing
+    }
+    const file = await getScreenshot(url, type);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', `image/${type}`);
+    res.end(file);
+};
