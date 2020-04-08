915f5a6f915e6b650520690d5afa227eede3d1ab
xcv58 <i@xcv58.com>
2020-04-07 20:24:10 -0700

    refactor

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-const getScreenshot = require('./costco');
-getScreenshot()
+require('dotenv').config()
+const isProd = process.env.NODE_ENV === 'production'
+const costco = require('./costco');
+const puppeteer = require(isProd ? 'puppeteer-core' : 'puppeteer')
+const chrome = isProd ? require('chrome-aws-lambda') : {
+  headless: process.env.HEADLESS !== 'false'
+}
+
+const init = async () => {
+  console.log('init browser');
+  const browser = isProd ? await puppeteer.launch({
+    dumpio: true,
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  }) : await puppeteer.launch(chrome)
+
+  const costcoRes = await costco(browser, '94102')
+  console.log(costcoRes);
+  await browser.close()
+}
+
+init()
---
+++ util.js
@@ -0,0 +1,32 @@
+const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4100.3 Safari/537.36'
+
+const fillForm = async (page, selector, content, BCount = 0) => {
+  console.log('Wait for selector:', selector)
+  const element = await page.waitForSelector(selector)
+  console.log('Type in this selector:', selector)
+  for (let i = 0; i < BCount; i++) {
+    await element.press('Backspace');
+  }
+  await element.type(content)
+}
+
+const click = async (page, selector) => {
+  console.log('Wait for selector:', selector)
+  const button = await page.waitForSelector(selector)
+  console.log('Click:', selector)
+  return await button.click()
+}
+
+const getNewPage = async (browser) => {
+  const page = await browser.newPage()
+  // Must set the user agent to avoid website denies connection from HeadlessChrome, thanks https://medium.com/@jsoverson/how-to-bypass-access-denied-pages-with-headless-chrome-87ddd5f3413c
+  await page.setUserAgent(userAgent)
+  return page
+}
+
+module.exports = {
+  userAgent,
+  fillForm,
+  click,
+  getNewPage
+}
