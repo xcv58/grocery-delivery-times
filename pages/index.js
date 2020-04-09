52bdb1b2d841d4fbea279440f793eb124d73f63d
xcv58 <i@xcv58.com>
2020-04-08 21:44:50 -0700

    Add tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,18 @@
 export default () => {
-  return <div>test</div>
+  return (
+    <div className="flex flex-wrap items-center justify-center p-1">
+      <h1 className="w-full text-3xl text-center">Grocery delivery times</h1>
+      <div>
+        <input
+          className="w-48 px-4 py-2 leading-normal bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:shadow-outline"
+          placeholder="zip code"
+          id="zip"
+          name="zip"
+          type="text"
+          inputmode="numeric"
+          pattern="^(?(^00000(|-0000))|(\d{5}(|-\d{4})))$"
+        />
+      </div>
+    </div>
+  )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {},
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+  },
+  plugins: [],
+}
