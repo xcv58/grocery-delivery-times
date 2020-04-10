243be5109235870036b4737573abcb786e3218ff
xcv58 <i@xcv58.com>
2020-04-10 16:53:10 -0700

    Add check for zip code on /api/github/*

--- github/getOrCreateIssue.js
+++ github/getOrCreateIssue.js
@@ -1,5 +1,6 @@
 import searchIssue from './searchIssue'
 import createIssue from './createIssue'
+import { isValidZip } from '../util'
 
 export const getOrCreateIssue = async ({ zip, website }) => {
   const data = await searchIssue({ zip, website })
@@ -11,6 +12,11 @@ export const getOrCreateIssue = async ({ zip, website }) => {
 
 export default async (req, res) => {
   const { zip, website } = req.query
+  if (!isValidZip(zip)) {
+    return res.status(400).json({
+      error: `Invalid zip code: '${zip}'`,
+    })
+  }
   const data = await getOrCreateIssue({ zip, website })
   res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
   res.json(data)
