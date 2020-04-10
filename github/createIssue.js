f778b7a0df6a7dc68ee49e4ec5cc69385cd4801c
xcv58 <i@xcv58.com>
2020-04-10 01:40:40 -0700

    Add issue body to indicate how to subscribe

--- github/createIssue.js
+++ github/createIssue.js
@@ -7,4 +7,5 @@ export default async ({ zip, website = ALL }) =>
     repo,
     title: `${zip} for ${website}`,
     labels: [zip, website],
+    body: `Please click the Subscribe button on the right bottom to get notification.\nThe bot will comment on the issue when there is time slot for ${zip}.`,
   })
