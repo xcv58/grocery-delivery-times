77182b572e96912b1aead542779fc57bdb927164
xcv58 <i@xcv58.com>
2020-04-12 17:52:59 -0700

    Handle invalid zip code on UI

--- pages/[zip].js
+++ pages/[zip].js
@@ -4,11 +4,15 @@ import Websites from '../components/Websites'
 import Website from '../components/Website'
 import useGA from '../components/useGA'
 import { ALL_WEBSITES } from '../util/websites'
+import { isValidZip } from '../util/index'
 
 export default () => {
   useGA()
   const router = useRouter()
   const { zip } = router.query
+  if (!isValidZip(zip)) {
+    return `Invalid zip code: ${zip}!`
+  }
   const sites = ALL_WEBSITES.map((website) => (
     <Website key={website} {...{ website, zip }} />
   ))
